8368dff1d838f1c08e4fe0964f3189cb10c1eb2f
brentgroves <brent.groves@gmail.com>
2025-10-13 19:13:07 -0400

    updated source code

--- volumes/python/oam/datetimet1.py
+++ volumes/python/oam/datetimet1.py
@@ -25,8 +25,11 @@ from IPython.display import display
 df = pd.read_csv('https://raw.githubusercontent.com/m-mehdi/pandas_tutorials/main/server_util.csv', parse_dates=['datetime'])
 # print(df.head())
 
-# df[df.server_id == 100].resample('D')['cpu_utilization', 'free_memory', 'session_count'].mean()
-df=df[df.server_id == 100]
+# Set time column as index
+df = df.set_index('datetime')
+
+df=df[df.server_id == 100].resample('D')['cpu_utilization', 'free_memory', 'session_count'].mean()
+# df=df[df.server_id == 100]
 print(df)
-# df[df.server_id == 100].resample('D')['cpu_utilization', 'free_memory', 'session_count'].mean()
-df.resample('D')['cpu_utilization', 'free_memory', 'session_count'].mean()
+# # df[df.server_id == 100].resample('D')['cpu_utilization', 'free_memory', 'session_count'].mean()
+# df.resample('D')['cpu_utilization', 'free_memory', 'session_count'].mean()
